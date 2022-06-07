1f1f20c9f1bf442e515c076305a4825135465aed
Jess Martin <jessmartin@gmail.com>
2022-06-07 13:50:08 -0400

    Setting up the basics

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '$lib/styles/style.css';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>A Danvers Fleury and Jess Martin project.</footer>
