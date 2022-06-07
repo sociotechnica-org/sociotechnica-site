4a7e9f9fea2ab3d98139541340b4336f3c0a656a
Jess Martin <jessmartin@gmail.com>
2022-06-07 16:24:58 -0400

    Adding new text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,36 @@
-<h1 class="text-center text-4xl mb-5">Resourcing Open Source</h1>
+<h1 class="text-center text-4xl mb-5">Resourcing The Commons</h1>
 <article class="leading-relaxed text-lg">
-	<p class="text-xl mb-5">
-		We believe that Open-source software maintenance is a poorly architected system that results in
-		chronic under-resourcing.
+	<p class="text-xl mb-3">
+		We believe that software creation and maintenance is a poorly architected system that drives
+		chronic project under-resourcing, which leads to a range of problems:
 	</p>
+	<ul class="list-disc mb-3">
+		<li class="ml-8">
+			<p>general instability in a piece of humanity's core infrastructure</p>
+		</li>
+		<li class="ml-8">
+			<p>wasting tens of thousands of developer hours per year</p>
+		</li>
+		<li class="ml-8">
+			<p>creating unnecessary barriers to entry for new software developers</p>
+		</li>
+	</ul>
+
 	<p>
-		We believe that the solution lies in a think-and-do lab that pioneers economic systems, with
-		novel processes and incentives. We think it works something like this:
+		<i class="text-neutral-500">
+			Read more about our analysis of the problem <a
+				class="border-b-2 hover:border-b-2 hover:border-yellow-700 font-semibold text-neutral-500 hover:text-yellow-700"
+				href="#">here</a
+			>.
+		</i>
 	</p>
-	<ul class="list-decimal">
+
+	<h2 class="text-2xl mt-5 mb-3">Proposed Solution</h2>
+	<p class="mb-3">
+		We believe that the solution lies in a think-and-do lab that pioneers new economic systems, with
+		novel processes and incentives. Our plan:
+	</p>
+	<ul class="list-decimal mb-5">
 		<li class="ml-8">
 			<p>
 				Run funding experiments following best practices to understand operational realities and
@@ -17,12 +39,51 @@
 		</li>
 		<li class="ml-8">
 			<p>
-				Create a bespoke industry segmentation map of engaged and prospective maintainers, funders
-				and developers.
+				Create an industry segmentation map of engaged and prospective maintainers, funders and
+				developers.
 			</p>
 		</li>
 		<li class="ml-8">
-			<p>Assay adjacent and unrelated industries for great ideas.</p>
+			<p>Explore adjacent and unrelated industries to find great ideas.</p>
+		</li>
+		<li class="ml-8">
+			<p>
+				Publish our findings, including analysis of the present, ideas for a more sustainable
+				future, and hypotheses for how to get there.
+			</p>
+		</li>
+		<li class="ml-8">
+			<p>
+				Create a prioritized queue of experiments and begin testing our way to a brighter future.
+			</p>
 		</li>
 	</ul>
+
+	<p>
+		<i class="text-neutral-500">
+			Read more about our approach to solving the problem <a
+				class="border-b-2 hover:border-b-2 hover:border-yellow-700 font-semibold text-neutral-500 hover:text-yellow-700"
+				href="#">here</a
+			> (coming soon).
+		</i>
+	</p>
+
+	<h2 class="text-2xl mt-5 mb-3">About SocioTechnica</h2>
+	<p class="mb-3">SocioTechnica is Danvers Fleury and Jess Martin.</p>
+	<p class="mb-3">
+		<a
+			class="border-b-2 hover:border-b-2 hover:border-yellow-700  font-semibold text-neutral-500 hover:text-yellow-700"
+			href="https://www.linkedin.com/in/danversfleury/">Danvers Fleury</a
+		> has contributed to 4 startup exits to acquirers Virgin, LendingTree, Oracle & Arthrex. He also
+		led a marketing agency for 8 years, and held the title of world’s Most Innovative MBA. He knows how
+		to cook one thing from scratch: pizza.
+	</p>
+	<p class="mb-5">
+		<a
+			class="border-b-2 hover:border-b-2 hover:border-yellow-700  font-semibold text-neutral-500 hover:text-yellow-700"
+			href="https://jessmart.in">Jess Martin</a
+		> has led teams to design, develop, and deploy dozens of products for companies spanning from startups
+		to the Fortune 100. He learned how to code in Basic on an Apple II at seven years old and has been
+		hooked ever since.
+	</p>
 </article>
