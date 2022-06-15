cc9da56791edceeed0e2acfc7b74c8d3b347b8fa
Jess Martin <jessmartin@gmail.com>
2022-06-14 21:27:38 -0400

    Styling up the Resources page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,7 @@
 <h1 class="text-center text-4xl mb-5">About SocioTechnica</h1>
 
-<p class="mb-3">SocioTechnica is Danvers Fleury and Jess Martin.</p>
-<p class="mb-3">
+<p class="mb-3 text-xl">SocioTechnica is Danvers Fleury and Jess Martin.</p>
+<p class="mb-3 text-lg">
   <a
     class="border-b-2 hover:border-b-2 hover:border-yellow-700  font-semibold text-neutral-500 hover:text-yellow-700"
     href="https://www.linkedin.com/in/danversfleury/">Danvers Fleury</a
@@ -9,7 +9,7 @@
   a marketing agency for 8 years, and held the title of world’s Most Innovative MBA. He knows how to
   cook one thing from scratch: pizza.
 </p>
-<p class="mb-5">
+<p class="mb-5 text-lg">
   <a
     class="border-b-2 hover:border-b-2 hover:border-yellow-700  font-semibold text-neutral-500 hover:text-yellow-700"
     href="https://jessmart.in">Jess Martin</a
--- src/routes/resources/index.svelte
+++ src/routes/resources/index.svelte
@@ -22,22 +22,42 @@
 </script>
 
 <script lang="ts">
+  import Resource from './_resource.svelte';
+
   export let resources: any;
 </script>
 
 <h1 class="text-center text-4xl mb-5">Resources</h1>
 
-<p class="text-xl mb-3">
-  The following resources are a living bibliography of the work on resourcing the commons.
+<p class="text-lg mb-3">
+  The following resources are a living bibliography of key resources related to resourcing the
+  commons. These notes are shared in the spirit of "working in public," not as a definitive list,
+  but rather as a way of sharing our perspective.
 </p>
 
 <ul>
+  <li class="hidden md:flex md:gap-1 pb-2">
+    <span class="pr-5 underline">Type</span>
+    <span class="flex-grow self-stretch underline">Title</span>
+    <span class="self-end underline">Author</span>
+  </li>
   {#each resources as resource}
-    <li>
-      <span class="{resourceColor(resource.meta.resourceType)} text-white p-1 rounded-sm pl-2 pr-2"
+    <li class="md:flex gap-1">
+      <span
+        class="{resourceColor(
+          resource.meta.resourceType
+        )} text-white p-1 pt-2 rounded-md pl-2 pr-2 inline-block md:flex uppercase font-sans text-sm"
         >{resource.meta.resourceType}</span
       >
-      <a href={resource.path}>{resource.meta.title}</a>
+      <span class="self-stretch flex-grow text-lg p-1 block md:flex">
+        <a
+          class="border-b-2 hover:border-b-2 hover:border-yellow-700  hover:text-yellow-700"
+          href={resource.path}>{resource.meta.title}</a
+        >
+      </span>
+      <span class="self-end p-1 pr-0 text-neutral-500 md:text-neutral-900"
+        >{resource.meta.author}</span
+      >
     </li>
   {/each}
 </ul>
