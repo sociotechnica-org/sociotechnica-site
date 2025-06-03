299ca0e7488575ea6f15b07794f77054aade1603
Jess Martin <jessmartin@gmail.com>
2025-06-03 11:03:55 -0400

    fix typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,22 @@ module.exports = {
         'urania': ['Urania Czech', 'monospace'],
         'typewriter': ['Urania Czech', 'monospace'],
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            'a': {
+              'color': 'inherit',
+              'text-decoration': 'none',
+              'border-bottom': '2px solid #e5e7eb',
+              'font-weight': 'inherit',
+              '&:hover': {
+                'color': '#a16207',
+                'border-bottom-color': '#a16207',
+              },
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
